3b132ff32f56f6d893e1c927427b2f0c7f2c49bf
intekor <valentin.m@intekor.ca>
2020-08-20 15:42:36 +0100

    structuration du projet

--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
 import Vue from 'vue'
-import Dashboard from './views/Dashboard.vue'
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import App from './App.vue'
+
+// Tailwind
+import '@/assets/css/main.css'
+
+// Styles: SCSS
+import '@/assets/scss/main.scss'
+
+// Styles: CSS
+import '@/assets/css/style.css'
+
+// Vue Router
+import router from './router'
+
 Vue.config.productionTip = false
 
-const routes = {
-  '/dashboard': Dashboard
-}
-
 new Vue({
-  data: {
-    currentRoute: window.location.pathname
-  },
-  computed: {
-    ViewComponent () {
-      return routes[this.currentRoute] || Dashboard
+  router,
+  data () {
+    return {
+      messageStr: 'Hello'
     }
   },
-  render (h) { return h(this.ViewComponent) },
+  render: h => h(App),
 }).$mount('#app')
